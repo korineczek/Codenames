a83329dff0eca4891d896876eb1080692c0992b1
Jiri Korinek <kormaister@gmail.com>
2016-02-11 13:46:15 +0100

    UI improvements, final fixes

--- Assets/Gameplay.cs
+++ Assets/Gameplay.cs
@@ -15,44 +15,26 @@ public class Gameplay : NetworkBehaviour {
     [SyncVar] public int ScoreBlue = 0;
     [SyncVar] public bool GameStarted = false;
     [SyncVar] public int StartingTeam = 3;
-
     private Interface gameUI;
 
-	// Use this for initialization
-	void Start ()
-	{
-        //get IPv4 address for client joining purposes
-        Debug.Log(Dns.GetHostEntry(Dns.GetHostName()).AddressList.FirstOrDefault(a => a.AddressFamily == AddressFamily.InterNetwork));
-	}
-	
-	// Update is called once per frame
-	void Update () {
-        Debug.Log(ScoreRed+ "  "+ ScoreBlue);
-
-	    if (Input.GetKeyUp(KeyCode.G))
-	    {
-            //Turn on UI monitoring for server and client
-	        gameUI = GameObject.Find("UI").GetComponent<Interface>();
-	        gameUI.StartCoroutine("UiRefresh");
-            RpcUiRefresh();
-
-            //Generate Board
-            Debug.Log("generating shit");
-            //Set starting teams
-	        StartingTeam = Random.Range(0, 2);
-
-            board = GetComponent<Board>().GenerateGrid(board, 10, StartingTeam);
-            GetComponent<Board>().SpawnBoard(board, this.transform);
-
-	        List<string> testlist = GetComponent<Board>().ProcessWordList();
-	    }
-	}
-
-    [Server]
-    public void Delete(GameObject obj)
+    public void StartGame()
     {
-        NetworkIdentity.Destroy(obj);
+        //Turn on UI monitoring for server and client
+        gameUI = GameObject.Find("UI").GetComponent<Interface>();
+        gameUI.StartCoroutine("UiRefresh");
+        RpcUiRefresh();
+
+        //Generate Board
+        Debug.Log("generating shit");
+        //Set starting teams
+        StartingTeam = Random.Range(0, 2);
+
+        board = GetComponent<Board>().GenerateGrid(board, 10, StartingTeam);
+        GetComponent<Board>().SpawnBoard(board, this.transform);
+
+        List<string> testlist = GetComponent<Board>().ProcessWordList();
     }
+
 
     [ClientRpc]
     public void RpcUiRefresh()
